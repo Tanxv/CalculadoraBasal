ef281bb49999370d0579fa717f74f33a87066164
marco <95975637+Tanxv@users.noreply.github.com>
2024-02-28 10:27:00 -0300

    Calc.Basal funcional 2.0

---
+++ script.js
@@ -0,0 +1,44 @@
+//Como sigue siendo mi primer proyecto en este lenguaje no quiero quedarme sin mi hola mundo
+console.log('hola mundo')
+
+//declariacion
+const CALCULAR = document.getElementById('calcular');
+const ERROR = document.getElementById('error');
+const FLU = document.getElementById('flu');
+const MAN = document.getElementById('man');
+//funcion
+function calcFlujo(peso){
+    let resto = peso;
+    let flujo = 0;
+    if (resto>20){
+        let aux = resto-20;
+        flujo += aux*1;
+        resto -= aux;
+    } 
+    if (resto>10){
+        let aux = resto-10;
+        flujo += aux*2;
+        resto -= aux;
+    }
+    flujo += resto*4;
+    return flujo;
+}
+
+//la magia:
+CALCULAR.addEventListener('click', () => {
+    const DATO = document.getElementById('peso').value
+    //validamos que se cargue un dato:
+    if (DATO > 0){
+        ERROR.style.display = 'none'
+        let flujo = calcFlujo(DATO);
+        let mantenimiento = flujo*1.5;
+        FLU.innerHTML = flujo + ' cc/hr';
+        MAN.innerHTML = 'm+m/2 ' + mantenimiento + ' cc/hr';
+        FLU.style.display = 'block';
+        MAN.style.display = 'block';
+    } else {
+        ERROR.style.display = 'block';
+        FLU.style.display = 'none';
+        MAN.style.display = 'none';
+    }
+})
